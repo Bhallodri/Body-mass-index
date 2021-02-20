2c70cb73af2417ece90fb232da82f403b45b539f
Hallodri <bhallodri@gmail.com>
2021-02-20 19:37:06 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 77;
+        double growth = 1.70 * 1.70;
+        double bodyMassIndex = weight / growth;
+        System.out.println(bodyMassIndex) ;
+    }
+}
